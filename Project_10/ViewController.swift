e6aefbb4f445604573ac430bd87636cdf983dfcc
Subhrajyoti <subha.ionic3@gmail.com>
2020-07-12 21:51:01 +0530

    Initial collection view structure

--- Project_10/ViewController.swift
+++ Project_10/ViewController.swift
@@ -4,15 +4,28 @@
 //
 //  Created by Subhrajyoti Chakraborty on 11/07/20.
 //  Copyright © 2020 Subhrajyoti Chakraborty. All rights reserved.
-//
+
+
+//  Note: We have to set Estimate Size to none in storyboard for the collectionView
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        return cell
     }
 
 
